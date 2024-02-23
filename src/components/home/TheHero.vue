55a442e312a6f956bd46fa7081632c28c63d599b
Greeny <verdepertuz07@gmail.com>
2024-02-23 12:17:59 -0500

    hero-text

--- src/components/home/TheHero.vue
+++ src/components/home/TheHero.vue
@@ -2,10 +2,18 @@
 
 <template>
   <section>
-    <img
-      src="https://concepto.de/wp-content/uploads/2014/08/universo-e1551279332781.jpg"
-      alt=""
-    />
+    <img src="/images/hero.jpg" alt="" />
+    <div class="hero-text">
+      <h1>Pintando mis pensamientos</h1>
+      <p>
+        ¡Conoce a Meteora, la hechicera de los Colores! En su camino va
+        transformando colores en historias visuales. El viaje creativo de
+        Meteora es una aventura emocionante, siempre descubriendo nuevos
+        tesoros. Ella explora el mundo de la imaginación sin miedo. Con sus
+        ideas coloridas, quiere hacer del mundo un un poco más vibrante y
+        encantador.
+      </p>
+    </div>
   </section>
 </template>
 
@@ -18,5 +26,12 @@ img {
   height: 100%;
   width: 100%;
   object-fit: cover;
+}
+
+.hero-text {
+  position: absolute;
+  width: 40%;
+  left: 100px;
+  bottom: 100px;
 }
 </style>
